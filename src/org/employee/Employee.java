30ad34942193da425f64b0e729bae8f364cde852
vvsprakash <v.v.s.prakash143@gmail.com>
2022-05-31 22:24:18 +0530

    newcode

---
+++ src/org/employee/Employee.java
@@ -0,0 +1,14 @@
+package org.employee;
+
+public class Employee {
+	public void empId() {
+		System.out.println("empid is 20");
+
+	}
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.empId();
+	}
+	
+
+}

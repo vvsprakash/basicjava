e0739df9588b809ca81579ad4fdb49758915ffa7
vvsprakash <v.v.s.prakash143@gmail.com>
2022-05-31 22:33:00 +0530

    update

---
+++ src/org/client/Client.java
@@ -0,0 +1,19 @@
+package org.client;
+
+import org.employee.Employee;
+
+public class Client {
+	public void empId() {
+		System.out.println("empid is 100");
+		System.out.println("1000");
+		System.out.println("");
+
+	}
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.empId();
+		e.empId();
+	}
+
+
+}
